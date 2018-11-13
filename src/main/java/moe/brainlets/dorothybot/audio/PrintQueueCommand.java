f7ce4cdbcd7e06e05e0388c89103a447b1123a43
Umang Patel <patel.umang5@gmail.com>
2018-11-13 14:17:43 -0500

    Add rate limit prevention in PrintQueueCommand

--- src/main/java/moe/brainlets/dorothybot/audio/PrintQueueCommand.java
+++ src/main/java/moe/brainlets/dorothybot/audio/PrintQueueCommand.java
@@ -1,9 +1,11 @@
 package moe.brainlets.dorothybot.audio;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import moe.brainlets.dorothybot.Command;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import sx.blah.discord.util.RequestBuffer;
 
 public class PrintQueueCommand implements Command {
 
@@ -16,17 +18,24 @@ public class PrintQueueCommand implements Command {
 			return;
 		}
 
-		// send titles/numbers to channel, 10 at a time
+		
+		List<String> messages = new ArrayList<String>();
 		String message = "";
 		for (int i = 0; i < titles.size(); i++) {
 			message += (i + 1) + ": " + titles.get(i) + "\n";
-			if (i % 10 == 9) {
-				event.getChannel().sendMessage(message);
+			if (i % 50 == 49) {
+				messages.add(message);
 				message = "";
 			}
 		}
-		if ((titles.size() - 1) % 10 != 9)
-			event.getChannel().sendMessage(message);
+		if ((titles.size() - 1) % 50 != 49)
+			messages.add(message);
+		
+		RequestBuffer.request(() -> {
+			for(String m: messages) {
+				event.getChannel().sendMessage(m);
+			}
+		});
 	}
 
 }
